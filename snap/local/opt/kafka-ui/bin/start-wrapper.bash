f82d1f7842ac30cd367869d78fd474c4fe48e835
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2025-07-24 17:02:30 +0100

    chore: bump to 1.3.0

--- snap/local/opt/kafka-ui/bin/start-wrapper.bash
+++ snap/local/opt/kafka-ui/bin/start-wrapper.bash
@@ -6,4 +6,4 @@ set -e
     --clear-groups \
     --reuid _daemon_ \
     --regid _daemon_ -- \
-    ${SNAP}/usr/lib/jvm/java-21-openjdk-amd64/bin/java -Dspring.config.additional-location=${SNAP_DATA}/etc/kafka-ui/application-local.yml --add-opens java.rmi/javax.rmi.ssl=ALL-UNNAMED $JAVA_OPTS -jar ${SNAP}/opt/kafka-ui/libs/api-1.2.0.jar
+    ${SNAP}/usr/lib/jvm/java-21-openjdk-amd64/bin/java -Dspring.config.additional-location=${SNAP_DATA}/etc/kafka-ui/application-local.yml --add-opens java.rmi/javax.rmi.ssl=ALL-UNNAMED $JAVA_OPTS -jar ${SNAP}/opt/kafka-ui/libs/api-1.3.0.jar
